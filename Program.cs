f2e0d3374c1f051c3db499e8b101d7d6c5dc99d4
jazmy <jazmy@JAZ-LAPTOP>
2025-05-07 16:20:22 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿static bool IsPalindrome(string x, int start, int end)
+{
+    if (start >= end)
+    {
+        return true;
+    }
+    else if (x[start] != x[end])
+    {
+        return false;
+    }
+    return IsPalindrome(x, start+1, end-1);
+}
+
+Console.Write("Please enter word to determine if it is a palindrome: ");
+string x=Console.ReadLine().ToLower();
+bool result = IsPalindrome(x,0, x.Length-1);
+
+if (result)
+{
+    Console.WriteLine($"{x} is a palindrome");
+}
+else
+{
+    Console.WriteLine($"{x} is not a palindrome");
+}
+
+Console.ReadKey();
